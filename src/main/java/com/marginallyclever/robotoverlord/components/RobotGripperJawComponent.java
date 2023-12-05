aabf41be735e3539f2bbc92f1ad2f84c4fc5f5bb
Dan Royer <dan@marginallyclever.com>
2023-12-05 10:15:06 -0800

    comments

--- src/main/java/com/marginallyclever/robotoverlord/components/RobotGripperJawComponent.java
+++ src/main/java/com/marginallyclever/robotoverlord/components/RobotGripperJawComponent.java
@@ -1,4 +1,7 @@
 package com.marginallyclever.robotoverlord.components;
 
+/**
+ * Identifier so that systems can find each child jaw of a gripper.
+ */
 public class RobotGripperJawComponent extends Component {
 }
